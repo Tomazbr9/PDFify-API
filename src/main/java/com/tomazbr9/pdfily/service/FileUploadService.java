94b7861412c88b60f1e2c0416156910d5ebb0dd8
tomaz <tomazcraft@gmail.com>
2025-11-13 01:11:18 -0300

    feat(FileUploadService): adiciona logs e comentários

--- src/main/java/com/tomazbr9/pdfily/service/FileUploadService.java
+++ src/main/java/com/tomazbr9/pdfily/service/FileUploadService.java
@@ -1,5 +1,7 @@
 package com.tomazbr9.pdfily.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -15,13 +17,16 @@ import java.util.UUID;
 @Service
 public class FileUploadService {
 
+    private static final Logger logger = LoggerFactory.getLogger(FileUploadService.class);
+
     @Value("${pdfily.upload.temp-dir}")
     private String uploadDir;
 
     public void uploadFile(MultipartFile file, UserDetails user){
 
         if(file == null || file.isEmpty()) {
-            throw new RuntimeException("O arquivo está vazio ou é nulo");
+            logger.warn("O arquivo enviado está vazio ou é nulo");
+            throw new RuntimeException("O arquivo enviado está vazio ou é nulo");
         }
 
         Path dirPath = Paths.get(uploadDir);
@@ -31,13 +36,20 @@ public class FileUploadService {
                 Files.createDirectories(dirPath);
             }
 
+            // Gera um nome seguro e praticamente único para o arquivo que será salvo.
             String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
+            // Resolve o nome do arquivo em relação ao diretório, produzindo o caminho completo onde o arquivo será escrito.
             Path filePath = dirPath.resolve(filename);
+
+            // Copia o conteúdo do MultipartFile para o filePath.
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
-        } catch (IOException e) {
-            throw new RuntimeException("Falha ao salvar arquivo temporário", e);
+            logger.info("Arquivo '{}' salvo temporariamente em: {}", file.getOriginalFilename(), filePath);
+
+        } catch (IOException error) {
+            logger.error("Erro ao salvar o arquivo '{}'", file.getOriginalFilename(), error);
+            throw new RuntimeException("Falha ao salvar arquivo temporário", error);
         }
 
     }
